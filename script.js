644a63442eb5755f09103ed1bd3fb9c8dd385332
Xplorer360 <107266773+Xplorer360@users.noreply.github.com>
2022-07-21 15:32:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background ="red";
+    event.target.style.color="white";
+});
